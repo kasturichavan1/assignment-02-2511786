6061e6b829554f2ea9bcf60e33479adafed36446
kasturichavan1 <kasturi.chavan07@gmail.com>
2026-03-23 19:04:14 +0530

    Update dw_queries.sql

--- part3-datawarehouse/dw_queries.sql
+++ part3-datawarehouse/dw_queries.sql
@@ -1,1 +1,24 @@
+-- Q1
 
+SELECT category,
+SUM(revenue)
+FROM fact_sales
+JOIN dim_product
+USING(product_id)
+GROUP BY category;
+-- Q2
+
+SELECT store_id,
+SUM(revenue)
+FROM fact_sales
+GROUP BY store_id
+ORDER BY SUM(revenue) DESC
+LIMIT 2;
+-- Q3
+
+SELECT month,
+SUM(revenue)
+FROM fact_sales
+JOIN dim_date
+USING(date_id)
+GROUP BY month;
